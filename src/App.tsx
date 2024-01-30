24029ab8ed81d29731dfa295f73ec3bf815fb401
yeonbot <tjdds1109@naver.com>
2024-01-30 22:02:05 +0900

    feat(GNU): Header & Logo 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import "./reset.css";
 import { CategoryList } from "./components/CategoryList";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <CategoryList />
     </div>
   );
